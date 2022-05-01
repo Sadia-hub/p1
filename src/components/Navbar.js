dc56a1a827dddbb480c1e34bcd0995fb97648ab9
Sadia <sadiashah059@gmail.com>
2022-05-01 13:53:32 -0200

    Link converted to NavLink
    It would help to apply styling to active links using css or js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,40 @@
 import styles from '../navbar.module.css'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+const NavStyles = ({isActive}) =>{
+    return isActive?{
+            fontWeight:"bold"
+        }:{
+            fontWeight:"normal"
+        }
+    
+}
 
 const Navbar = () =>{
     return<nav>
     <ul style={{listStyle:"none", display:"flex", marginInline:20, alignItems:"center",justifyContent:"center", textDecoration:"none"}}>
         <li style={{margin:10, textDecoration:"none"}}>
-            <Link to="/"> Home </Link>           
+            {/* Navlink helps to find the active links. So, we can apply styles to them using 
+            javascript or css
+            */}
+            <NavLink 
+            style={NavStyles}
+            to="/"> Home </NavLink>           
         </li>
         <li style={{margin:10}}>
-            <Link to="/projects"> Projects </Link>    
+            <NavLink 
+            style={NavStyles}
+            to="/projects"> Projects </NavLink>    
         </li>
         <li style={{margin:10}}>
-            <Link to="/resume"> Download Resume </Link>    
+            <NavLink
+            style={NavStyles}
+            to="/resume"> Download Resume </NavLink>    
         </li>
         <li style={{margin:10}}>
-            <Link to="/contact"> Contacts </Link>    
+            <NavLink 
+            style={NavStyles}
+            to="/contact"> Contacts </NavLink>    
         </li>
     
     </ul>
