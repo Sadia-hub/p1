e098aaef2ca10d07dc337818a79179fc96c68f21
Sadia <sadiashah059@gmail.com>
2022-05-06 14:05:54 -0200

    use reducer hook

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import './App.css';
 import React, { createContext } from 'react';
-import A from './components/A';
 
 const userContext = createContext();
 
 function App() {
   return (
     <div className="App">
-      <h1>Use Context Hook</h1>
-      <h5>A technique to pass data through the component tree without having to pass props down manually at each level</h5>
+      <h1>Use Reducer Hook</h1>
 
-      <h5>It can be through context API as well. But it has higher code complexity. So, use context hook helps in simplifying the code</h5>
-
-      <h5>In use Context, context creation and provision remains the same. It just changes the way of context consumption</h5>
-
-
-        <userContext.Provider value="Sadia">
-          <A />
-        </userContext.Provider>
+      <h4>Alternative to use state. It is a state manaement tool</h4>
+      <h4>Use State is built using Use Reducer</h4>
 
     </div>
   );
 }
 
 export default App;
-export {userContext};
+
 
 
--- src/components/C.js
+++
@@ -1,9 +0,0 @@
-import D from "./D";
-const C = () =>{
-    return <>
-    <h1>Component C calls D</h1>
-    <D/>
-    </>
-}
-
-export default C;
