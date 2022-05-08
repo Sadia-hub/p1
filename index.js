8c8117ca36d264617e4b316d808b08c6ecb351f5
Sadia <sadiashah059@gmail.com>
2022-05-08 18:51:19 -0200

    immer done

--- index.js
+++ index.js
@@ -1,101 +1,60 @@
 const redux=require('redux');
-const createStore = redux.createStore;
-//alternative way to dispatch actions is bind action creators. However, it's an old technique. Not necessary these
-//days
 const bindActionCreators = redux.bindActionCreators
-const combineReducers = redux.combineReducers
+const createStore = redux.createStore;
+
+//immer helps in updating state as a mutable object
+const produce = require('immer').produce
 
 
-
-const CAKE_ORDERED = "CAKE_ORDERED";
-const CAKE_RESTOCKED = "CAKE_RESTOCKED";
-//adding other two actions
-const ICE_CREAM_ORDERED = "ICE_CREAM_ORDERED";
-const ICE_CREAM_RESTOCKED = "ICE_CREAM_RESTOCKED";
+//define action type
+const STREET_UPDATED = "STREET_UPDATED";
 
 
 
 //define action creator
-const orderCake = ()=>{
-    return {
-        type:CAKE_ORDERED,
-        payload:1
+const updateStreet = street =>{
+   return{ 
+        type:STREET_UPDATED,
+        payload:street
     }
 }
 
-const restockCake = (qty=1)=>{
-    return {
-        type:CAKE_RESTOCKED,
-        payload:qty
-    }
-}
-
-const orderIceCream = () =>{
-    return {
-        type:ICE_CREAM_ORDERED,
-        payload:1
-    }
-}
-
-const restockIceCream = (qty)=>{
-    return {
-        type:ICE_CREAM_RESTOCKED,
-        payload:qty
-    }
-}
 
 //define state
-const initialCakeState = {
-    noOfCakes:10,
-    otherProperties:10
-}
-
-const initialIceCreamState = {
-    noOfIceCreams:10
+const initialState = {
+    name:"Sadia",
+    contact:"0238976543",
+    address:{
+        street:"AVS colony",
+        city:"Mirpur Mathelo"
+    }
 }
 
 //define cake reducer
-const cakeReducer = (state=initialCakeState, action) =>{
+const reducer = (state=initialState, action) =>{
     switch(action.type){
-        case CAKE_ORDERED:
-            return{
-                ...state,
-                noOfCakes:state.noOfCakes-1
-            }
-        case CAKE_RESTOCKED:
-            return{
-                ...state,
-                noOfCakes:state.noOfCakes+action.payload
-            }
+        case STREET_UPDATED:
+            // return{
+            //     ...state,
+            //     address:{
+            //         ...state.address,
+            //         street:action.payload
+            //     }
+            // }
+
+            //above code is done using immer
+            //accepts two arguments: current state and a callback with draft object as parameter
+            return produce(state,(draft)=>{
+                draft.address.street = action.payload
+            })
         default:
             return state
     }
 }
 
-//ice cream reducer
-const iceCreamReducer = (state=initialIceCreamState,action)=>{
-    switch(action.type){
-        case ICE_CREAM_ORDERED:
-            return{
-                ...state,
-                noOfIceCreams:state.noOfIceCreams-1
-            }
-        case ICE_CREAM_RESTOCKED:
-            return{
-                ...state,
-                noOfIceCreams:state.noOfIceCreams+action.payload
-            }
-        default:
-            return state
-    }
-}
 
-const rootReducer = combineReducers({
-    cake:cakeReducer,
-    iceCream:iceCreamReducer
-});
 
-const store=createStore(rootReducer);
+const store=createStore(reducer);
 
 console.log("INITIAL STATE", store.getState());
 
@@ -103,20 +62,8 @@ const unsubscribe = store.subscribe(()=>{
     console.log("Updated state: ",store.getState())
 })
 
-// store.dispatch(orderCake())
-// store.dispatch(orderCake())
-// store.dispatch(orderCake())
-// store.dispatch(restockCake(3))
 
-const actions = bindActionCreators({orderCake,restockCake, orderIceCream, restockIceCream},store.dispatch)
-actions.orderCake();
-actions.orderCake();
-actions.orderCake();
-actions.restockCake(3);
-
-actions.orderIceCream();
-actions.orderIceCream();
-actions.orderIceCream();
-actions.restockIceCream(3);
+const actions = bindActionCreators({updateStreet},store.dispatch)
+actions.updateStreet("ABC mall road")
 
 unsubscribe()
