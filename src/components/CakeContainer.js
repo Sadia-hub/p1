3d7f6409eebe43cc7ae0d153df2a6b00c348a710
Sadia <sadiashah059@gmail.com>
2022-05-14 03:05:22 -0200

    redux toolkit

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,8 +1,26 @@
-const CakeContainer = () =>{
+import { buyCake } from "../redux"
+import { connect } from "react-redux"
+const CakeContainer = ({noOfCakes, buyCake}) =>{
     return <>
-    <h1>Numer of Cakes</h1>
-    <button>Buy a Cake</button>
+    <h1>Numer of Cakes {noOfCakes}</h1>
+    <button
+    onClick={buyCake}
+    >Buy a Cake</button>
     </>
 }
 
-export default CakeContainer;
+//this will provide a noOfCake prop to the CakeCOntainer Component
+const mapStateToProps = state =>{
+    return {
+        noOfCakes:state.noOfCakes
+    }
+}
+
+//this prop would dispatch the buyCake action
+const mapDispatchToProps = dispatch =>{
+    return {
+        buyCake:()=>dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CakeContainer);
