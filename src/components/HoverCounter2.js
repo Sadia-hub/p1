d84c44277431d328ed54367c92cd7a251bcc2a7b
Sadia <sadiashah059@gmail.com>
2022-07-24 05:31:35 -0200

    code refactored

--- src/components/HoverCounter2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HoverCounter2({count, increment}) {
-  return (
-    <div onMouseOver={increment}>You hovered {count} times</div>
-  )
-}
-
-export default HoverCounter2
